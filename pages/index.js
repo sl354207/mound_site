688ae3e616b5bf73f78394b9b900d805e970b5b8
sl354207 <sl354207@ohio.edu>
2021-06-07 15:17:27 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
 
       <main>

ace9bd97c3735a7c72ac5eb9ccada1d3d23b127b
sl354207 <sl354207@ohio.edu>
2021-07-28 11:08:32 -0400

    added basic responsive image gallery

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,22 @@
-import Meta from './Meta'
-import Nav from './Nav'
+import Meta from "./Meta";
+import Nav from "./Nav";
 // import Header from './Header'
 // import styles from '../styles/Layout.module.css'
 
 // pass in children as a prop so any components in Layout tag in _app.js get rendered inside Layout
-const Layout = ({children}) => {
-    return (
-        <>
-            <Meta />
-            <Nav />
-            <div>
-                <main>
-                    {/* <Header /> */}
-                    {children}
-                </main>
-            </div>
-        </>
-    )
-}
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Meta />
+      <Nav />
+      <div>
+        <main>
+          {/* <Header /> */}
+          {children}
+        </main>
+      </div>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -1,5 +1,93 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { Button, Grid, Paper, Typography } from "@material-ui/core";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const gallery = () => {
-  return <div></div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Paper className={classes.paper}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="300"
+                  image="https://res.cloudinary.com/dr3hjsjiq/image/upload/v1626201646/sample.jpg"
+                  title="Contemplative Reptile"
+                />
+              </CardActionArea>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Paper className={classes.paper}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="300"
+                  image="https://res.cloudinary.com/dr3hjsjiq/image/upload/v1626201646/sample.jpg"
+                  title="Contemplative Reptile"
+                />
+              </CardActionArea>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Paper className={classes.paper}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="300"
+                  image="https://res.cloudinary.com/dr3hjsjiq/image/upload/v1626201646/sample.jpg"
+                  title="Contemplative Reptile"
+                />
+              </CardActionArea>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Paper className={classes.paper}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="300"
+                  image="https://res.cloudinary.com/dr3hjsjiq/image/upload/v1626201646/sample.jpg"
+                  title="Contemplative Reptile"
+                />
+              </CardActionArea>
+            </Card>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default gallery;
